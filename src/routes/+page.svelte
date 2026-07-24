e768fbc2a8da0201a7e2e6d4b9143c55b3c91f4e
Him1nshu <astng888@gmail.com>
2026-07-24 17:37:32 +0530

    feat(players): implement player list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let winr=$state("");
+	let plyrs=$state(["magnus","padmananda","unniksrihsnan"]);
+	function addinlist() {
+		plyrs=[...plyrs,winr];
+		winr="";
+	}
+	
+</script>
+<style>
+    h1{
+        color:red;
+    }
+	h2{
+		color: green;
+	}
+</style>
+<h1>CHESS TOURNAMENT</h1>
+<hr>
+<h2>add in the new player name</h2>
+
+<input type="text" placeholder="enter short name" bind:value={(winr)}>
+<button onclick={addinlist}>add in name</button>
+<hr>
+<ol>
+	
+{#each plyrs as p}
+	<li><h3>{p}</h3></li>
+{/each}
+</ol>
+
+
